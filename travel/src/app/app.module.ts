af4b0037660a7904a66a11d1597bcdb3887cacd5
at-hungtran <hung.tran@asiantech.vn>
2018-09-12 13:43:21 +0700

    add share component

--- travel/src/app/app.module.ts
+++ travel/src/app/app.module.ts
@@ -2,13 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ShareModule } from './share/share.module';
+
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ShareModule
   ],
   providers: [],
   bootstrap: [AppComponent]
