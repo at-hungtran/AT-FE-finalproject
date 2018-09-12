b719de29f0ca66998fc15964907ff938a600b3ee
at-vynguyen <vy.nguyen@asiantech.vn>
2018-09-12 18:46:39 +0700

    end_task

--- travel/src/app/share/component/card-article/card-article.component.ts
+++ travel/src/app/share/component/card-article/card-article.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-article',
-  templateUrl: './article.component.html'
+  templateUrl: './card-article.component.html'
 })
 
 export class ArticleComponent {}
