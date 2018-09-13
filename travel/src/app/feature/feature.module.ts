90473d35aa9e7e0618fce0f02128bdd3436cd55d
at-vynguyen <vy.nguyen@asiantech.vn>
2018-09-13 17:45:38 +0700

    add site page 2

--- travel/src/app/feature/feature.module.ts
+++ travel/src/app/feature/feature.module.ts
@@ -2,8 +2,6 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { FeatureComponent } from './feature.component';
-import { HomeModule } from './home/home.module';
-import { SiteModule } from './site/site.module';
 import { FeatureRoutingModule } from './feature-routing.module';
 import { ShareModule } from '../share/share.module';
 
@@ -14,13 +12,8 @@ import { ShareModule } from '../share/share.module';
   imports: [
     CommonModule,
     RouterModule,
-    HomeModule,
-    SiteModule,
     FeatureRoutingModule,
     ShareModule
-  ],
-  exports: [
-    HomeModule
   ]
 })
 
--- travel/src/app/feature/site/site.module.ts
+++ travel/src/app/feature/site/site.module.ts
@@ -1,6 +1,9 @@
 import { NgModule } from '@angular/core';
 import { SiteComponent } from './site.component';
 import { CommonModule } from '@angular/common';
+import { SiteRoutingModule } from './site-routing.module';
+import { ShareModule } from '../../share/share.module';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [
@@ -8,6 +11,9 @@ import { CommonModule } from '@angular/common';
   ],
   imports: [
     CommonModule,
+    RouterModule,
+    SiteRoutingModule,
+    ShareModule
   ],
   exports: [
   ],
