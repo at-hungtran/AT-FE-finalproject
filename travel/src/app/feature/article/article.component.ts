03a5603ae2cfc29019f8769a7f6e6d919bc16422
at-vynguyen <vy.nguyen@asiantech.vn>
2018-09-14 18:13:47 +0700

    fix UI page Article

---
+++ travel/src/app/feature/article/article.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-article-page',
+  templateUrl: './article.component.html'
+})
+
+export class ArticlePageComponent {}
--- travel/src/app/feature/destination/destination.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-destination-page',
-  templateUrl: './destination.component.html'
-})
-
-export class DestinationPageComponent {}
