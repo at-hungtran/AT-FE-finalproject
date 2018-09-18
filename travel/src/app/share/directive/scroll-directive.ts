dd92726f5175b841b6841e57154a5c9828952654
at-hungtran <hung.tran@asiantech.vn>
2018-09-18 16:33:51 +0700

    fix scroll

--- travel/src/app/share/directive/scroll-directive.ts
+++ travel/src/app/share/directive/scroll-directive.ts
@@ -6,6 +6,8 @@ import { Directive, HostListener, ElementRef, Renderer2 } from '@angular/core';
 
 export class ScrollDirective {
   lastScrollTop = 0;
+  lastScrollDown = 0;
+  lastScrollUp = 0;
 
   constructor(private el: ElementRef, private renderer: Renderer2) {}
 
@@ -13,10 +15,15 @@ export class ScrollDirective {
   scrolling() {
     const iCurScrollPos  = window.pageYOffset;
     const headerHeight = this.el.nativeElement.clientHeight;
-    if (iCurScrollPos > this.lastScrollTop) {
-      this.renderer.addClass(this.el.nativeElement, 'hide-nav');
-      this.renderer.removeClass(this.el.nativeElement, 'show-nav');
+
+    if (iCurScrollPos > this.lastScrollTop && iCurScrollPos > 500) {
+      this.lastScrollUp = iCurScrollPos;
+      if (this.lastScrollUp - this.lastScrollDown > 150) {
+        this.renderer.addClass(this.el.nativeElement, 'hide-nav');
+        this.renderer.removeClass(this.el.nativeElement, 'show-nav');
+      }
     } else if (iCurScrollPos < this.lastScrollTop) {
+      this.lastScrollDown = iCurScrollPos;
       this.renderer.addClass(this.el.nativeElement, 'show-nav');
       this.renderer.removeClass(this.el.nativeElement, 'hide-nav');
     }
