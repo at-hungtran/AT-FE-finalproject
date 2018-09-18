3db687acddbdf14122a341eb187c137f622daa86
at-vynguyen <vy.nguyen@asiantech.vn>
2018-09-18 16:34:00 +0700

    build_login_client

--- travel/src/app/feature/login/login.component.ts
+++ travel/src/app/feature/login/login.component.ts
@@ -1,8 +1,35 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from "@angular/forms";
+import { Router } from '@angular/router';
+import { AuthService } from '../../share/service/auth.service';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html'
 })
 
-export class LoginComponent {}
+export class LoginComponent implements OnInit {
+  form: FormGroup;
+  messagePerErrorCode = {
+    loginfailed: "Invalid credentials"
+  };
+  error = [];
+  constructor(private fb: FormBuilder, private router: Router, private auth: AuthService) {
+    this.form = this.fb.group({
+      userName: ['', Validators.required],
+      password: ['', Validators.required]
+    })
+  }
+
+  ngOnInit() {
+  }
+
+  login() {
+    const val = this.form.value;
+    if(val.userName && val.password) {
+      this.auth.login(val.userName, val.password).subscribe(() => {
+        console.log('User is logged in');
+        this.router.navigateByUrl('/');
+      });
+    }
+  }
+}
